0304307ee09f25e0546c23dd3064630905d867b6
s13146 <s13146@pjwstk.edu.pl>
2020-06-10 19:00:42 +0200

    naprawa case i parametrow metody

--- src/Game.java
+++ src/Game.java
@@ -1,12 +1,12 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 public class Game {
-    static public void zgadywanie(String word1, int chance) {
-        char[] scaner = new char[word1.length()]; // przypisuje do zmiennej długość słowa
+    static public void zgadywanie(String word, int chance) {
+        char[] scaner = new char[word.length()]; // przypisuje do zmiennej długość słowa
         int i = 0;
-        while (i < word1.length()) {
+        while (i < word.length()) {
             scaner[i] = '-';
-            if (word1.charAt(i) == ' ') { //zwraca znak znajdujacy sie pod wskazanym indeksem
+            if (word.charAt(i) == ' ') { //zwraca znak znajdujacy sie pod wskazanym indeksem
                 scaner[i] = ' ';
             }
             i++;
@@ -17,7 +17,7 @@ public class Game {
         Scanner s = new Scanner (System.in); // wczytywanie liter
         ArrayList<Character> l = new ArrayList <Character> (); //tu wpisywac bedziemy litery
 
-        while (chance >0) {
+        while (chance>0) {
             char x=s.next().charAt(0); // znak wczytywany przez uzytkownika
             if(l.contains(x)) {
                 System.out.println("Powtórzony znak");
@@ -26,9 +26,9 @@ public class Game {
             l.add(x);
 
 
-            if (word1.contains(x+" ")) {
-                for (int y=0; y<word1.length(); y++) {  //petal sprawdza indeksy
-                    if (word1.charAt(y)==x) { //zamiana znaku '-'
+            if (word.contains(x+" ")) {
+                for (int y=0; y<word.length(); y++) {  //petal sprawdza indeksy
+                    if (word.charAt(y)==x) { //zamiana znaku '-'
                         scaner[y]=x;  //na litere
                     }
                 }
@@ -37,7 +37,7 @@ public class Game {
                 chance--;  //inktementacja zmiennej gdy błędne litery
             }
 
-            if (word1.equals(String.valueOf(scaner))) { //  sprawdza czy wygrana
+            if (word.equals(String.valueOf(scaner))) { //  sprawdza czy wygrana
                 System.out.println(scaner);
                 System.out.println("Zgadłeś! Wygrałeś!");
                 break; //przerywa petle
@@ -111,34 +111,38 @@ public class Game {
         System.out.println("|    \\"); //and so using 2 tell Java to just print the 2nd
     }
 
+
     public static void main(String[] args) {
         System.out.println("GRA WISIELEC");
         System.out.println("Wybierz poziom");
         System.out.println("1.Łatwy");
         System.out.println("2.Średni");
         System.out.println("3.Trudny ");
+        System.out.println("Wybieram poziom: ");
         Scanner scanner = new Scanner(System.in);
         int level = scanner.nextInt();
-
-        String word1 = " TEST";  //słowo
-        String word2 = " WARUNEK";  //słowo
-        String word3 = " DEINKREMENTACJA";  //słowo
+        String word;
         int chance = 5;  // ilość żyć
         switch (level) {
             case 1:
                 System.out.println("Poziom łatwy");
-                zgadywanie(word1, chance);
-
+                String easy = "TEST";  //słowo
+                word= easy;
+                zgadywanie(word, chance);
                 break;
             case 2:
                 System.out.println("Poziom średni");
-                zgadywanie(word2, chance);
-
+                String midi = "WARUNEK";  //słowo
+                word=  midi;
+                zgadywanie(word, chance);
                 break;
-            default:
+            case 3:
                 System.out.println("Poziom trudny");
-                zgadywanie(word3, chance);
-
+                String hard = "DEINKREMENTACJA";  //słowo
+                word= hard;
+                zgadywanie(word, chance);
+            default:
+                System.out.println("Zły wybór");
         }
 
     }
