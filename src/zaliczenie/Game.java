66a9b2a603fee901258120d234ae1c6d11f08bd1
s13146 <s13146@pjwstk.edu.pl>
2020-06-13 09:00:29 +0200

    poprawki rysowanie szubienic

--- src/zaliczenie/Game.java
+++ src/zaliczenie/Game.java
@@ -6,11 +6,66 @@ import java.util.Random;
 
 public class Game {
     private char[] word;
-    static public void zgadywanie (String word, int chance) {
+    public static void man_death()
+    {
+        System.out.println("_____");
+        System.out.println("|   |");
+        System.out.println("|   o");
+        System.out.println("|  /|\\"); //why the double \\, because the \ has special meaning
+        System.out.println("|  / \\"); //and so using 2 tell Java to just print the 2nd
+    }
+
+    public static void man_0() {
+        System.out.println("_____");
+        System.out.println("|   |");
+        System.out.println("|   ");
+        System.out.println("|   ");
+        System.out.println("|   ");
+    }
+    public static void man_1() {
+        System.out.println("_____");
+        System.out.println("|   |");
+        System.out.println("|   o");
+        System.out.println("|    "); //why the double \\, because the \ has special meaning
+        System.out.println("|    "); //and so using 2 tell Java to just print the 2nd
+    }
+
+    public static void man_2() {
+        System.out.println("_____");
+        System.out.println("|   |");
+        System.out.println("|   o");
+        System.out.println("|   | "); //why the double \\, because the \ has special meaning
+        System.out.println("|    "); //and so using 2 tell Java to just print the 2nd
+    }
+
+    public static void man_3() {
+        System.out.println("_____");
+        System.out.println("|   |");
+        System.out.println("|   o");
+        System.out.println("|  /| "); //why the double \\, because the \ has special meaning
+        System.out.println("|    "); //and so using 2 tell Java to just print the 2nd
+    }
+
+    public static void man_4() {
+        System.out.println("_____");
+        System.out.println("|   |");
+        System.out.println("|   o");
+        System.out.println("|  /|\\ "); //why the double \\, because the \ has special meaning
+        System.out.println("|    "); //and so using 2 tell Java to just print the 2nd
+    }
+    public static void man_5() {
+        System.out.println("_____");
+        System.out.println("|   |");
+        System.out.println("|   o");
+        System.out.println("|  /|\\ "); //why the double \\, because the \ has special meaning
+        System.out.println("|    \\"); //and so using 2 tell Java to just print the 2nd
+    }
+
+    static public void playGame (String word, int chance) {
         char[] scaner = new char[word.length()]; // przypisuje do zmiennej długość słowa
         int i = 0;
         while (i < word.length()) {
-            scaner[i] = '_';
+            scaner[i] = '-';
             if (word.charAt(i) == ' ') { //zwraca znak znajdujacy sie pod wskazanym indeksem
                 scaner[i] = ' ';
             }
@@ -18,6 +73,7 @@ public class Game {
         }
         System.out.println(scaner);
         System.out.println("Pozostało żyć = " + chance);
+
 
 
         Scanner s = new Scanner(System.in); // wpisywanie liter
@@ -42,6 +98,19 @@ public class Game {
                 }
             } else {
                 chance--;  //inktementacja zmiennej gdy błędne litery
+                switch(chance){
+                    case 1:
+                        man_5();
+                    case 2:
+                        man_4();
+                    case 3:
+                        man_3();
+                    case 4 :
+                        man_2();
+                    case 5:
+                        man_1();
+
+                }
             }
 
             if (word.equals(String.valueOf(scaner))) { //  sprawdza czy wygrana
@@ -52,13 +121,14 @@ public class Game {
             System.out.println(scaner);
             System.out.println("Pozostało żyć = " + chance);
 
-            System.out.println("Koło ratunkowe! Wciśnij T");
+            /*System.out.println("Koło ratunkowe! Wciśnij T");
             Scanner scanner = new Scanner(System.in);
-            int help = scanner.nextInt();
+            int help = scanner.nextInt(); */
 
 
             if (chance == 0) {
                 System.out.println("Koniec szans, przegrałeś"); //zero szans = przegrana
+                man_death();
             }
         }
     }
@@ -77,64 +147,9 @@ public class Game {
 
             }
     }*/
-    public void man_death()
-    {
-        System.out.println("_____");
-        System.out.println("|   |");
-        System.out.println("|   o");
-        System.out.println("|  /|\\"); //why the double \\, because the \ has special meaning
-        System.out.println("|  / \\"); //and so using 2 tell Java to just print the 2nd
-    }
 
-    public void man_0() {
-        System.out.println("_____");
-        System.out.println("|   |");
-        System.out.println("|   ");
-        System.out.println("|   "); //why the double \\, because the \ has special meaning
-        System.out.println("|   "); //and so using 2 tell Java to just print the 2nd
-    }
-
-    public void man_1() {
-        System.out.println("_____");
-        System.out.println("|   |");
-        System.out.println("|   o");
-        System.out.println("|    "); //why the double \\, because the \ has special meaning
-        System.out.println("|    "); //and so using 2 tell Java to just print the 2nd
-    }
-
-    public void man_2() {
-        System.out.println("_____");
-        System.out.println("|   |");
-        System.out.println("|   o");
-        System.out.println("|   | "); //why the double \\, because the \ has special meaning
-        System.out.println("|    "); //and so using 2 tell Java to just print the 2nd
-    }
-
-    public void man_3() {
-        System.out.println("_____");
-        System.out.println("|   |");
-        System.out.println("|   o");
-        System.out.println("|  /| "); //why the double \\, because the \ has special meaning
-        System.out.println("|    "); //and so using 2 tell Java to just print the 2nd
-    }
-
-    public void man_4() {
-        System.out.println("_____");
-        System.out.println("|   |");
-        System.out.println("|   o");
-        System.out.println("|  /|\\ "); //why the double \\, because the \ has special meaning
-        System.out.println("|    "); //and so using 2 tell Java to just print the 2nd
-    }
-    public void man_5() {
-        System.out.println("_____");
-        System.out.println("|   |");
-        System.out.println("|   o");
-        System.out.println("|  /|\\ "); //why the double \\, because the \ has special meaning
-        System.out.println("|    \\"); //and so using 2 tell Java to just print the 2nd
-    }
 
     public static void main(String[] args) {
-        Words Word;
         System.out.println("GRA WISIELEC");
         System.out.println("Wybierz poziom");
         System.out.println("1.Łatwy");
@@ -144,22 +159,28 @@ public class Game {
         Scanner scanner = new Scanner(System.in);
         int level = scanner.nextInt();
         String word;
-        int chance = 5;  // ilość szans
+        int chance = 6;  // ilość szans
         switch (level) {
             case 1:
                 System.out.println("Poziom łatwy");
-                word = Word.getRandomEasyWord();
-                zgadywanie(word,chance);
+                String[] easy = {"TEST", "KOD", "STYL", "KLASA", "OBIEKT", "BLOK", "IMPORT", "LISTA", "TYP", "STOS"};  //tablica ze słowami
+                Random random1= new Random();
+                word = easy[random1.nextInt(easy.length)];
+                playGame(word,chance);
                 break;
             case 2:
                 System.out.println("Poziom średni");
-                word = Word.getRandomMidiWord();
-                zgadywanie(word,chance);
+                String[] midi = {"WARUNEK" , "INTERFEJS", "PAKIET", "KOLEKCJA", "APLIKACJA", "PARAMETR", "TABLICA", "LAMBDA", "HIERARCHIA", "ZASADY"};
+                Random random2= new Random();
+                word = midi[random2.nextInt(midi.length)];
+                playGame(word,chance);
                 break;
             case 3:
                 System.out.println("Poziom trudny");
-                word = Word.getRandomHardWord();
-                zgadywanie(word,chance);
+                String[] hard =  {"DEINKREMENTACJA","INKREMENTACJA", "POLIANDROM", "DZIEDZICZENIE", "REPOZYTORIUM", "POLIMORFIZM", "KONSTRUKTOR", "DOKUMENTACJA", "HERMETYZACJA", "REFERENCJA"};
+                Random random3= new Random();
+                word = hard[random3.nextInt(hard.length)];
+                playGame(word,chance);
             default:
                 System.out.println("Zły wybór");
         }
